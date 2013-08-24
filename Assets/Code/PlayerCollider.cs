a5b3e2e47a674817a90e3893bd52ca280b04a046
SadMonk <SadMonk@gmx.de>
2013-08-24 21:21:09 +0200

    added a drop object... somehow the collision doesn't work yet... the tutorial I found failed me.

---
+++ Assets/Code/PlayerCollider.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollider : MonoBehaviour {
+	
+	void OnCollisionEnter (Collision col)
+    {
+		Debug.Log("Collision with: " + col.gameObject.name);
+        if(col.gameObject.name == "Drop")
+        {
+			//PickUpBuff(buff);
+            Destroy(col.gameObject);
+        }
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
