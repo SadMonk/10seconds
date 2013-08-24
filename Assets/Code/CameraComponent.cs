1027470ea4d4a642d5b06918181d38c757929c5b
Jeff Zimmer <Jeff.Zimmer@hyunkell.com>
2013-08-24 13:29:19 +0200

    absolute basics

--- Assets/Code/CameraComponent.cs
+++ Assets/Code/CameraComponent.cs
@@ -9,10 +9,14 @@ public class CameraComponent : MonoBehaviour
     /// Size of a game space unit in pixels
     /// </summary>
     public int UnitSize;
+    public GameObject FollowTarget;
 
     void Update()
     {
         camera.orthographicSize = (float)Screen.height / (float)UnitSize / 2.0f;
+
+        if( FollowTarget != null )
+            transform.position = new Vector3( FollowTarget.transform.position.x, FollowTarget.transform.position.y, transform.position.z );
     }
 
     public Vector3 Project( Vector3 screenPosition ) { return camera.ScreenPointToRay( screenPosition ).origin; }
---
+++ Assets/Code/PlayerControl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent( typeof( PlayerStats ) )]
+public class PlayerControl : MonoBehaviour
+{
+    PlayerStats stats;
+
+    // Use this for initialization
+    void Start()
+    {
+        stats = GetComponent<PlayerStats>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( Input.GetKey( KeyCode.LeftArrow ) )
+        {
+            transform.Translate( new Vector3( -stats.MovementSpeed * Time.deltaTime, 0, 0 ) );
+        }
+
+        if( Input.GetKey( KeyCode.RightArrow ) )
+        {
+            transform.Translate( new Vector3( stats.MovementSpeed * Time.deltaTime, 0, 0 ) );
+        }
+
+        if( Input.GetKey( KeyCode.UpArrow ) )
+        {
+            transform.Translate( new Vector3( 0, stats.MovementSpeed * Time.deltaTime, 0 ) );
+        }
+
+        if( Input.GetKey( KeyCode.DownArrow ) )
+        {
+            transform.Translate( new Vector3( 0, -stats.MovementSpeed * Time.deltaTime, 0 ) );
+        }
+    }
+}
