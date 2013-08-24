b5cf20f4e8184b3856b2ce9337755627e708ae35
Jeff Zimmer <Jeff.Zimmer@hyunkell.com>
2013-08-24 16:52:59 +0200

    Z level fix

---
+++ Assets/Code/ColliderAxisConstraint.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColliderAxisConstraint : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        Vector3 pos = transform.position;
+        transform.position = new Vector3( pos.x, pos.y, 0 );
+	}
+}
--- Assets/Code/MovementComponent.cs
+++ Assets/Code/MovementComponent.cs
@@ -6,15 +6,18 @@ public class MovementComponent : MonoBehaviour
     public float MovementSpeed;
     public Vector3 Velocity;
 
+    public const float ZLevelMultiplyer = 0.001f;
+
 	// Use this for initialization
 	void Start () {
-	
+        
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate ()
     {
         rigidbody.velocity = Velocity * MovementSpeed;
+        transform.position = new Vector3( transform.position.x, transform.position.y, transform.position.y * ZLevelMultiplyer );
 	}
 
     public void Move( float x, float y )
