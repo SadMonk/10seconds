77b52152a1212e04f6b7d5d75b17802c924ff6ea
SadMonk <SadMonk@gmx.de>
2013-08-26 12:13:02 +0200

    Buff-Icons adjusted

---
+++ Assets/Code/GameHUD.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameHUD : MonoBehaviour {
+ 
+    public Texture2D strengthIcon;
+    public Texture2D attackSpeedIcon;
+    public Texture2D walkingSpeedIcon;
+    public Texture2D skinThicknessIcon;
+    public Texture2D chainLightningIcon;
+    public Texture2D spikesIcon;
+    public Texture2D magnetIcon;
+    public Texture2D whirlwindIcon;
+    public Texture2D trapIcon;
+    
+	void OnGUI() 
+    {
+        GUILayout.BeginArea(new Rect(0,0,Screen.width,50));
+        
+        GUILayout.Label("Score:");
+        
+        GUILayout.EndArea();
+    }
+}
